5d4ffd9a174a3b2127d17cc55c5e4d3f219bcc2e
Lucky Mehra <luckymehra@ksu.edu>
2019-06-17 15:10:27 -0500

    inital commit

---
+++ src/install_packages.R
@@ -0,0 +1,11 @@
+# install packages
+
+if (!require(pacman)){
+	install.packages("pacman")
+}
+
+# packages needed for this project
+pacman::p_load(tidyverse, 
+							 readxl, 
+							 openxlsx,
+							 readODS)
